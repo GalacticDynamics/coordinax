6a44e73b1a56da7502bc4a5905acb607c9539d5f
nstarman <nstarman@users.noreply.github.com>
2024-02-11 14:15:52 -0500

    fix: registrations of convert quantity
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/vector/_utils.py
+++ src/vector/_utils.py
@@ -8,6 +8,7 @@ from typing import Any
 import array_api_jax_compat as xp
 import astropy.units as u
 import jax.numpy as jnp
+from jax.dtypes import canonicalize_dtype
 from jax_quantity import Quantity
 from jaxtyping import Array, Float, Shaped
 
@@ -22,11 +23,13 @@ def converter_quantity_array(x: Any, /) -> Float[Quantity, "*shape"]:
 @converter_quantity_array.register
 def _convert_quantity(x: Quantity, /) -> Float[Quantity, "*shape"]:
     """Convert to a batched vector."""
-    return xp.asarray(x, dtype=jnp.promote_types(x.dtype, float))
+    dtype = jnp.promote_types(x.dtype, canonicalize_dtype(float))
+    return xp.asarray(x, dtype=dtype)
 
 
-@converter_quantity_array.register
+@converter_quantity_array.register(jnp.ndarray)
 def _convert_jax_array(x: Shaped[Array, "*shape"], /) -> Float[Quantity, "*shape"]:
     """Convert to a batched vector."""
-    x = xp.asarray(x, dtype=jnp.promote_types(x.dtype, float))
+    dtype = jnp.promote_types(x.dtype, canonicalize_dtype(float))
+    x = xp.asarray(x, dtype=dtype)
     return Quantity(x, u.one)
