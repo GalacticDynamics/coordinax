51495bc2816f50ebcff0bb3519c1c12190b813d2
nstarman <nstarman@users.noreply.github.com>
2024-02-19 15:24:12 -0500

    feat: Abstract1DVectorDifferential
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/vector/_d1/base.py
+++ src/vector/_d1/base.py
@@ -1,13 +1,18 @@
 """Representation of coordinates in different systems."""
 
-__all__ = ["Abstract1DVector"]
+__all__ = ["Abstract1DVector", "Abstract1DVectorDifferential"]
 
-from typing import TypeVar
 
-from vector._base import AbstractVector
+import equinox as eqx
 
-T = TypeVar("T", bound="AbstractVector")
+from vector._base import AbstractVector, AbstractVectorDifferential
 
 
 class Abstract1DVector(AbstractVector):
     """Abstract representation of 1D coordinates in different systems."""
+
+
+class Abstract1DVectorDifferential(AbstractVectorDifferential):
+    """Abstract representation of 1D differentials in different systems."""
+
+    vector_cls: eqx.AbstractClassVar[type[Abstract1DVector]]
