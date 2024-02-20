e8bcae17b1b7ab4d4a5540880054251eaa69fe14
nstarman <nstarman@users.noreply.github.com>
2024-02-20 16:06:57 -0500

    feat: runtime type checking
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/vector/__init__.py
+++ src/vector/__init__.py
@@ -5,16 +5,22 @@
 vector: Vectors in JAX
 """
 
-from . import _base, _d1, _d2, _d3, _exceptions, _transform, _typing, _utils
-from ._base import *
-from ._d1 import *
-from ._d2 import *
-from ._d3 import *
-from ._exceptions import *
-from ._transform import *
-from ._typing import *
-from ._utils import *
+from jaxtyping import install_import_hook
+
 from ._version import version as __version__
+from .setup_package import RUNTIME_TYPECHECKER
+
+with install_import_hook("vector", RUNTIME_TYPECHECKER):
+    from ._base import *
+    from ._d1 import *
+    from ._d2 import *
+    from ._d3 import *
+    from ._exceptions import *
+    from ._transform import *
+    from ._typing import *
+    from ._utils import *
+
+from . import _base, _d1, _d2, _d3, _exceptions, _transform, _typing, _utils
 
 __all__ = ["__version__"]
 __all__ += _base.__all__
@@ -28,4 +34,4 @@ __all__ += _utils.__all__
 
 
 # Cleanup
-del _base, _exceptions, _transform, _typing, _utils, _d1, _d2, _d3
+del _base, _exceptions, _transform, _typing, _utils, _d1, _d2, _d3, RUNTIME_TYPECHECKER
