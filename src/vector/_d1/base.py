40e3c2bf2e7a69b09cf03f011f2b30fa51b6e2e9
nstarman <nstarman@users.noreply.github.com>
2024-02-28 22:22:56 -0500

    feat: constructor for 0D quantity
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/vector/_d1/base.py
+++ src/vector/_d1/base.py
@@ -4,6 +4,11 @@ __all__ = ["Abstract1DVector", "Abstract1DVectorDifferential"]
 
 
 from abc import abstractmethod
+from dataclasses import fields
+
+from jaxtyping import Shaped
+
+from jax_quantity import Quantity
 
 from vector._base import AbstractVector, AbstractVectorBase, AbstractVectorDifferential
 from vector._utils import classproperty
@@ -11,6 +16,27 @@ from vector._utils import classproperty
 
 class Abstract1DVector(AbstractVector):
     """Abstract representation of 1D coordinates in different systems."""
+
+    @classmethod
+    @AbstractVector.constructor._f.dispatch  # type: ignore[attr-defined, misc]  # noqa: SLF001
+    def constructor(
+        cls: type["Abstract1DVector"], x: Shaped[Quantity["length"], ""], /
+    ) -> "Abstract1DVector":
+        """Construct a 1D vector.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> from vector import Cartesian1DVector
+
+        >>> q = Cartesian1DVector.constructor(Quantity(1, "kpc"))
+        >>> q
+        Cartesian1DVector(
+           x=Quantity[PhysicalType('length')](value=f32[1], unit=Unit("kpc"))
+        )
+
+        """
+        return cls(**{fields(cls)[0].name: x.reshape(1)})
 
     @classproperty
     @classmethod
