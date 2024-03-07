9f868e09a1c49691d5d7de598b91fb929702abc6
nstarman <nstarman@users.noreply.github.com>
2024-03-07 13:55:58 -0500

    docs: add to registered call methods
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_d4/operate.py
+++ src/coordinax/_d4/operate.py
@@ -14,7 +14,32 @@ from coordinax.operators._base import AbstractOperator, op_call_dispatch
 
 @op_call_dispatch
 def call(self: AbstractOperator, x: FourVector, /) -> FourVector:
-    """Dispatch to the operator's `__call__` method."""
+    """Dispatch to the operator's `__call__` method.
+
+    Examples
+    --------
+    >>> from jax_quantity import Quantity
+    >>> import coordinax as cx
+
+    We can then create a spatial translation operator:
+
+    >>> op = cx.operators.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+    >>> op
+    GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+    We can then apply the operator to a position:
+
+    >>> pos = cx.FourVector.constructor(Quantity([0, 1.0, 2.0, 3.0], "kpc"))
+    >>> pos
+    FourVector( t=Quantity[PhysicalType('time')](...), q=Cartesian3DVector( ... ) )
+
+    >>> newpos = op(pos)
+    >>> newpos
+    FourVector( t=Quantity[PhysicalType('time')](...), q=Cartesian3DVector( ... ) )
+    >>> newpos.q.x
+    Quantity['length'](Array(2., dtype=float64), unit='kpc')
+
+    """
     q, t = self(x.q, x.t)
     return FourVector(t=t, q=q)
 
@@ -23,5 +48,28 @@ def call(self: AbstractOperator, x: FourVector, /) -> FourVector:
 def call(
     self: AbstractOperator, x: Shaped[Quantity["length"], "*batch 4"], /
 ) -> Shaped[Quantity["length"], "*batch 4"]:
-    """Dispatch to the operator's `__call__` method."""
+    """Dispatch to the operator's `__call__` method.
+
+    Examples
+    --------
+    >>> from jax_quantity import Quantity
+    >>> import coordinax as cx
+
+    We can then create a spatial translation operator:
+
+    >>> op = cx.operators.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+    >>> op
+    GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+    We can then apply the operator to a position:
+
+    >>> pos = Quantity([0, 1.0, 2.0, 3.0], "kpc")
+    >>> pos
+    Quantity['length'](Array([0., 1., 2., 3.], dtype=float64), unit='kpc')
+
+    >>> newpos = op(pos)
+    >>> newpos
+    Quantity['length'](Array([0., 2., 4., 6.], dtype=float64), unit='kpc')
+
+    """
     return convert(self(FourVector.constructor(x)), Quantity)
