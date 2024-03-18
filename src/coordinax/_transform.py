4ec9beee829f9adc6ca1beceee42811a1b4f0597
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-03-18 17:04:10 -0400

    feat: q as vec for p represent_as (#65)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_transform.py
+++ src/coordinax/_transform.py
@@ -30,38 +30,103 @@ from ._utils import dataclass_items
     (
         Abstract1DVectorDifferential,
         type[Abstract1DVectorDifferential],  # type: ignore[misc]
-        AbstractVector,
+        AbstractVector | Quantity["length"],
     ),
     (
         Abstract2DVectorDifferential,
         type[Abstract2DVectorDifferential],  # type: ignore[misc]
-        AbstractVector,
+        AbstractVector | Quantity["length"],
     ),
     (
         Abstract3DVectorDifferential,
         type[Abstract3DVectorDifferential],  # type: ignore[misc]
-        AbstractVector,
+        AbstractVector | Quantity["length"],
     ),
 )
 def represent_as(
     current: AbstractVectorDifferential,
     target: type[AbstractVectorDifferential],
-    position: AbstractVector,
+    position: AbstractVector | Quantity["length"],
     /,
     **kwargs: Any,
 ) -> AbstractVectorDifferential:
-    """Abstract3DVectorDifferential -> Cartesian -> Abstract3DVectorDifferential.
+    """AbstractVectorDifferential -> Cartesian -> AbstractVectorDifferential.
 
-    This is the base case for the transformation of 1D vector differentials.
+    This is the base case for the transformation of vector differentials.
+
+    Parameters
+    ----------
+    current : AbstractVectorDifferential
+        The vector differential to transform.
+    target : type[AbstractVectorDifferential]
+        The target type of the vector differential.
+    position : AbstractVector
+        The position vector used to transform the differential.
+    **kwargs : Any
+        Additional keyword arguments.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+    >>> from unxt import Quantity
+
+    Let's start in 1D:
+
+    >>> q = cx.Cartesian1DVector(x=Quantity(1.0, "km"))
+    >>> p = cx.CartesianDifferential1D(d_x=Quantity(1.0, "km/s"))
+    >>> cx.represent_as(p, cx.RadialDifferential, q)
+    RadialDifferential( d_r=Quantity[...]( value=f32[], unit=Unit("km / s") ) )
+
+    Now in 2D:
+
+    >>> q = cx.Cartesian2DVector.constructor(Quantity([1.0, 2.0], "km"))
+    >>> p = cx.CartesianDifferential2D.constructor(Quantity([1.0, 2.0], "km/s"))
+    >>> cx.represent_as(p, cx.PolarDifferential, q)
+    PolarDifferential(
+      d_r=Quantity[...]( value=f32[], unit=Unit("km / s") ),
+      d_phi=Quantity[...]( value=f32[], unit=Unit("rad / s") )
+    )
+
+    And in 3D:
+
+    >>> q = cx.Cartesian3DVector.constructor(Quantity([1.0, 2.0, 3.0], "km"))
+    >>> p = cx.CartesianDifferential3D.constructor(Quantity([1.0, 2.0, 3.0], "km/s"))
+    >>> cx.represent_as(p, cx.SphericalDifferential, q)
+    SphericalDifferential(
+      d_r=Quantity[...]( value=f32[], unit=Unit("km / s") ),
+      d_theta=Quantity[...]( value=f32[], unit=Unit("rad / s") ),
+      d_phi=Quantity[...]( value=f32[], unit=Unit("rad / s") )
+    )
+
+    If given a position as a Quantity, it will be converted to the appropriate
+    Cartesian vector:
+
+    >>> p = cx.CartesianDifferential3D.constructor(Quantity([1.0, 2.0, 3.0], "km/s"))
+    >>> cx.represent_as(p, cx.SphericalDifferential, Quantity([1.0, 2.0, 3.0], "km"))
+    SphericalDifferential(
+      d_r=Quantity[...]( value=f32[], unit=Unit("km / s") ),
+      d_theta=Quantity[...]( value=f32[], unit=Unit("rad / s") ),
+      d_phi=Quantity[...]( value=f32[], unit=Unit("rad / s") )
+    )
+
     """
     # TODO: not require the shape munging / support more shapes
     shape = current.shape
     flat_shape = prod(shape)
-    position = position.reshape(flat_shape)  # flattened
+
+    # Parse the position to an AbstractVector
+    if isinstance(position, AbstractVector):
+        posvec = position
+    else:  # Q -> Cart<X>D
+        posvec = current.integral_cls._cartesian_cls.constructor(  # noqa: SLF001
+            position
+        )
+
+    posvec = posvec.reshape(flat_shape)  # flattened
 
     # Start by transforming the position to the type required by the
     # differential to construct the Jacobian.
-    current_position = represent_as(position, current.integral_cls, **kwargs)
+    current_pos = represent_as(posvec, current.integral_cls, **kwargs)
 
     # Takes the Jacobian through the representation transformation function.  This
     # returns a representation of the target type, where the value of each field the
@@ -69,7 +134,7 @@ def represent_as(
     # the correct numerator unit (of the Jacobian row). The value is a Vector of the
     # original type, with fields that are the columns of that row, but with only the
     # denomicator's units.
-    jac_nested_vecs = jac_rep_as(current_position, target.integral_cls)
+    jac_nested_vecs = jac_rep_as(current_pos, target.integral_cls)
 
     # This changes the Jacobian to be a dictionary of each row, with the value
     # being that row's column as a dictionary, now with the correct units for
