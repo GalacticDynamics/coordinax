df37637c7fee8d02b5190e5102805cbc092213f8
nstarman <nstarman@users.noreply.github.com>
2024-03-19 12:46:21 -0400

    doc: operate
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_d3/operate.py
+++ src/coordinax/_d3/operate.py
@@ -18,9 +18,40 @@ Q3: TypeAlias = Shaped[Quantity["length"], "*#batch 3"]
 
 
 @op_call_dispatch
-def call(self: AbstractOperator, x: Q3, /) -> Q3:
-    """Dispatch to the operator's `__call__` method."""
-    return self(Cartesian3DVector.constructor(x))
+def call(self: AbstractOperator, q: Q3, /) -> Q3:
+    r"""Operate on a 3D Quantity.
+
+    `q` is the position vector. This is interpreted as a 3D CartesianVector.
+    See :class:`coordinax.Cartesian3DVector` for more details.
+
+    Returns
+    -------
+    x' : Quantity['length', '*#batch 3']
+        The operated-upon position vector.
+
+    Examples
+    --------
+    >>> from unxt import Quantity
+    >>> import coordinax as cx
+    >>> import coordinax.operators as cxo
+
+    >>> shift = Quantity([1.0, 2.0, 3.0], "kpc")
+    >>> op = cxo.GalileanSpatialTranslationOperator(shift)
+
+    >>> q = Quantity([0.0, 0, 0], "kpc")
+    >>> op(q)
+    Quantity['length'](Array([1., 2., 3.], dtype=float32), unit='kpc')
+
+    Since :meth:`AbstractOperator.__call__` uses multiple dispatch, we
+    explicitly call this registered method.
+
+    >>> op.__call__._f.resolve_method((op, q))[0](op, q)
+    Quantity['length'](Array([1., 2., 3.], dtype=float32), unit='kpc')
+
+    """
+    return convert(
+        self(Cartesian3DVector.constructor(q)).represent_as(Cartesian3DVector), Quantity
+    )
 
 
 @op_call_dispatch
@@ -42,10 +73,17 @@ def call(
 
     We can then apply the operator to a position:
 
-    >>> pos = Quantity([1.0, 2.0, 3.0], "kpc")
+    >>> q = Quantity([1.0, 2.0, 3.0], "kpc")
     >>> t = Quantity(0.0, "Gyr")
 
-    >>> op(pos, t)
+    >>> op(q, t)
+    (Quantity['length'](Array([2., 4., 6.], dtype=float32), unit='kpc'),
+        Quantity['time'](Array(0., dtype=float32, ...), unit='Gyr'))
+
+    Since :meth:`AbstractOperator.__call__` uses multiple dispatch, we
+    explicitly call this registered method.
+
+    >>> op.__call__._f.resolve_method((op, q, t))[0](op, q, t)
     (Quantity['length'](Array([2., 4., 6.], dtype=float32), unit='kpc'),
         Quantity['time'](Array(0., dtype=float32, ...), unit='Gyr'))
 
