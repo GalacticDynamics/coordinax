07fd54474703c35a0045117359db04009be4fdcb
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-04-02 15:39:44 -0400

    Fix lonlat (#84)
    * fix: LonLatSphericalVector transformations
    * feat: out-of-range lonlat constructor
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_d3/sphere.py
+++ src/coordinax/_d3/sphere.py
@@ -22,6 +22,8 @@ from typing import final
 import equinox as eqx
 import jax
 
+import quaxed.array_api as xp
+import quaxed.lax as qlax
 from unxt import Distance, Quantity
 
 import coordinax._typing as ct
@@ -33,6 +35,9 @@ from coordinax._checks import (
 )
 from coordinax._converters import converter_azimuth_to_range
 from coordinax._utils import classproperty
+
+_90d = Quantity(90, "deg")
+_180d = Quantity(180, "deg")
 
 ##############################################################################
 # Position
@@ -175,6 +180,9 @@ class MathSphericalVector(AbstractSphericalVector):
         return self.r
 
 
+# ============================================================================
+
+
 @final
 class LonLatSphericalVector(AbstractSphericalVector):
     """Spherical vector representation.
@@ -191,6 +199,47 @@ class LonLatSphericalVector(AbstractSphericalVector):
         The latitude (polar angle) [-90, 90] [deg] where 90 is the z-axis.
     distance : Distance
         Radial distance r (slant distance to origin),
+
+    Examples
+    --------
+    >>> from unxt import Quantity
+    >>> import coordinax as cx
+
+    >>> cx.LonLatSphericalVector(lon=Quantity(0, "deg"), lat=Quantity(0, "deg"),
+    ...                          distance=Quantity(3, "kpc"))
+    LonLatSphericalVector(
+      lon=Quantity[PhysicalType('angle')](value=f32[], unit=Unit("deg")),
+      lat=Quantity[PhysicalType('angle')](value=f32[], unit=Unit("deg")),
+      distance=Distance(value=f32[], unit=Unit("kpc"))
+    )
+
+    The longitude and latitude angles are in the range [0, 360) and [-90, 90] degrees,
+    and the radial distance is non-negative.
+    When initializing, the longitude is wrapped to the [0, 360) degrees range.
+
+    >>> vec = cx.LonLatSphericalVector(lon=Quantity(365, "deg"),
+    ...                                lat=Quantity(90, "deg"),
+    ...                                distance=Quantity(3, "kpc"))
+    >>> vec.lon
+    Quantity['angle'](Array(5., dtype=float32), unit='deg')
+
+    The latitude is not wrapped, but it is checked to be in the [-90, 90] degrees range.
+
+    >>> try:
+    ...     cx.LonLatSphericalVector(lon=Quantity(0, "deg"), lat=Quantity(100, "deg"),
+    ...                              distance=Quantity(3, "kpc"))
+    ... except Exception as e:
+    ...     print(e)
+    The inclination angle must be in the range [0, pi]...
+
+    Likewise, the radial distance is checked to be non-negative.
+
+    >>> try:
+    ...     cx.LonLatSphericalVector(lon=Quantity(0, "deg"), lat=Quantity(0, "deg"),
+    ...                              distance=Quantity(-3, "kpc"))
+    ... except Exception as e:
+    ...     print(e)
+    The radial distance must be non-negative...
 
     """
 
@@ -214,7 +263,7 @@ class LonLatSphericalVector(AbstractSphericalVector):
     def __check_init__(self) -> None:
         """Check the validity of the initialization."""
         check_azimuth_range(self.lon)
-        check_polar_range(self.lat)
+        check_polar_range(self.lat, -Quantity(90, "deg"), Quantity(90, "deg"))
         check_r_non_negative(self.distance)
 
     @classproperty
@@ -237,6 +286,102 @@ class LonLatSphericalVector(AbstractSphericalVector):
 
         """
         return self.distance
+
+
+@LonLatSphericalVector.constructor._f.register  # type: ignore[attr-defined, misc]  # noqa: SLF001
+def constructor(
+    cls: type[LonLatSphericalVector],
+    *,
+    lon: Quantity["angle"],
+    lat: Quantity["angle"],
+    distance: Distance,
+) -> LonLatSphericalVector:
+    """Construct LonLatSphericalVector, allowing for out-of-range values.
+
+    Examples
+    --------
+    >>> import coordinax as cx
+
+    Let's start with a valid input:
+
+    >>> cx.LonLatSphericalVector.constructor(lon=Quantity(0, "deg"),
+    ...                                      lat=Quantity(0, "deg"),
+    ...                                      distance=Quantity(3, "kpc"))
+    LonLatSphericalVector(
+      lon=Quantity[PhysicalType('angle')](value=f32[], unit=Unit("deg")),
+      lat=Quantity[PhysicalType('angle')](value=f32[], unit=Unit("deg")),
+      distance=Distance(value=f32[], unit=Unit("kpc"))
+    )
+
+    The distance can be negative, which wraps the longitude by 180 degrees and
+    flips the latitude:
+
+    >>> vec = cx.LonLatSphericalVector.constructor(lon=Quantity(0, "deg"),
+    ...                                            lat=Quantity(45, "deg"),
+    ...                                            distance=Quantity(-3, "kpc"))
+    >>> vec.lon
+    Quantity['angle'](Array(180., dtype=float32), unit='deg')
+    >>> vec.lat
+    Quantity['angle'](Array(-45., dtype=float32), unit='deg')
+    >>> vec.distance
+    Distance(Array(3., dtype=float32), unit='kpc')
+
+    The latitude can be outside the [-90, 90] deg range, causing the longitude
+    to be shifted by 180 degrees:
+
+    >>> vec = cx.LonLatSphericalVector.constructor(lon=Quantity(0, "deg"),
+    ...                                            lat=Quantity(-100, "deg"),
+    ...                                            distance=Quantity(3, "kpc"))
+    >>> vec.lon
+    Quantity['angle'](Array(180., dtype=float32), unit='deg')
+    >>> vec.lat
+    Quantity['angle'](Array(-80., dtype=float32), unit='deg')
+    >>> vec.distance
+    Distance(Array(3., dtype=float32), unit='kpc')
+
+    >>> vec = cx.LonLatSphericalVector.constructor(lon=Quantity(0, "deg"),
+    ...                                            lat=Quantity(100, "deg"),
+    ...                                            distance=Quantity(3, "kpc"))
+    >>> vec.lon
+    Quantity['angle'](Array(180., dtype=float32), unit='deg')
+    >>> vec.lat
+    Quantity['angle'](Array(80., dtype=float32), unit='deg')
+    >>> vec.distance
+    Distance(Array(3., dtype=float32), unit='kpc')
+
+    The longitude can be outside the [0, 360) deg range. This is wrapped to the
+    [0, 360) deg range (actually the base constructor does this):
+
+    >>> vec = cx.LonLatSphericalVector.constructor(lon=Quantity(365, "deg"),
+    ...                                            lat=Quantity(0, "deg"),
+    ...                                            distance=Quantity(3, "kpc"))
+    >>> vec.lon
+    Quantity['angle'](Array(5., dtype=float32), unit='deg')
+
+    """
+    # 1) Convert the inputs
+    fields = LonLatSphericalVector.__dataclass_fields__
+    lon = fields["lon"].metadata["converter"](lon)
+    lat = fields["lat"].metadata["converter"](lat)
+    distance = fields["distance"].metadata["converter"](distance)
+
+    # 2) handle negative distances
+    distance_pred = distance < xp.zeros_like(distance)
+    distance = qlax.select(distance_pred, -distance, distance)
+    lon = qlax.select(distance_pred, lon + _180d, lon)
+    lat = qlax.select(distance_pred, -lat, lat)
+
+    # 3) Handle latitude outside of [-90, 90] degrees
+    lat_pred = lat < -_90d
+    lat = qlax.select(lat_pred, -_180d - lat, lat)
+    lon = qlax.select(lat_pred, lon + _180d, lon)
+
+    lat_pred = lat > _90d
+    lat = qlax.select(lat_pred, _180d - lat, lat)
+    lon = qlax.select(lat_pred, lon + _180d, lon)
+
+    # 4) Construct. This also handles the longitude wrapping
+    return cls(lon=lon, lat=lat, distance=distance)
 
 
 ##############################################################################
