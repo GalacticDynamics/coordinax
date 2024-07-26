a51e5cbb34773d76a9a2e255543dfccc230413d4
nstarman <nstarman@users.noreply.github.com>
2024-07-25 21:23:55 -0400

    docs: use unitsystem in example
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_coordinax/base.py
+++ src/coordinax/_coordinax/base.py
@@ -705,13 +705,13 @@ class AbstractVector(ArrayValue):  # type: ignore[misc]
         Examples
         --------
         >>> import astropy.units as u
-        >>> from unxt import Quantity, UnitSystem
+        >>> from unxt import Quantity, unitsystem
         >>> import coordinax as cx
 
-        >>> units = UnitSystem(u.m, u.s, u.kg, u.rad)
+        >>> usys = unitsystem(u.m, u.s, u.kg, u.rad)
 
         >>> vec = cx.CartesianPosition3D.constructor([1, 2, 3], "km")
-        >>> vec.to_units(units)
+        >>> vec.to_units(usys)
         CartesianPosition3D(
             x=Quantity[...](value=f32[], unit=Unit("m")),
             y=Quantity[...](value=f32[], unit=Unit("m")),
