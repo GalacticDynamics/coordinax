5a4435b96a99fd244cbc29b2a6c8af24ce668f06
nstarman <nstarman@users.noreply.github.com>
2024-08-15 12:32:08 -0400

    refactor: rename galilean module
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/operators.py
+++ src/coordinax/operators.py
@@ -7,25 +7,25 @@ E.g. a translation.
 from ._coordinax.operators import (
     _base,
     _composite,
-    _galilean,
     _identity,
     _sequential,
     funcs,
+    galilean,
 )
 from ._coordinax.operators._composite import *  # noqa: F403
-from ._coordinax.operators._galilean import *  # noqa: F403
 from ._coordinax.operators._identity import *  # noqa: F403
 from ._coordinax.operators._sequential import *  # noqa: F403
 from ._coordinax.operators.base import *  # noqa: F403
 from ._coordinax.operators.funcs import *  # noqa: F403
+from ._coordinax.operators.galilean import *  # noqa: F403
 
 __all__: list[str] = []
 __all__ += _base.__all__
 __all__ += _composite.__all__
 __all__ += _sequential.__all__
 __all__ += _identity.__all__
-__all__ += _galilean.__all__
+__all__ += galilean.__all__
 __all__ += funcs.__all__
 
 # Cleanup
-del _base, _composite, funcs, _galilean, _identity, _sequential
+del _base, _composite, funcs, galilean, _identity, _sequential
