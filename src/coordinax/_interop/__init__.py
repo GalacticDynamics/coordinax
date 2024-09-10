e731c7f7e9d01050dc30864a6ed61ce22a25350b
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-09-10 02:18:14 -0400

    build: bump optional_dependencies (#178)
    * build: bump optional_dependencies
    * ci: satisfy pylint
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_interop/__init__.py
+++ src/coordinax/_interop/__init__.py
@@ -4,5 +4,5 @@ __all__: list[str] = []
 
 from .optional_deps import OptDeps
 
-if OptDeps.ASTROPY.is_installed:
+if OptDeps.ASTROPY.installed:
     from . import coordinax_interop_astropy  # noqa: F401
