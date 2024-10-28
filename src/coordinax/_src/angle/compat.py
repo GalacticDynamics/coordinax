8b6f4b8c110c15aebb849888c1362332fd52ea45
nstarman <nstarman@users.noreply.github.com>
2024-10-28 10:12:26 -0400

    feat: angles
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ src/coordinax/_src/angle/compat.py
@@ -0,0 +1,37 @@
+"""Compatibility for Quantity."""
+
+__all__: list[str] = []
+
+from plum import conversion_method
+
+from unxt import AbstractQuantity
+
+from .core import Angle
+
+
+@conversion_method(type_from=AbstractQuantity, type_to=Angle)  # type: ignore[misc]
+def _quantity_to_angle(q: AbstractQuantity, /) -> Angle:
+    """Convert any quantity to an Angle.
+
+    Examples
+    --------
+    >>> from plum import convert
+    >>> from unxt import Quantity, UncheckedQuantity
+    >>> from coordinax.angle import Angle
+    >>> q = UncheckedQuantity(1, "rad")
+    >>> q
+    UncheckedQuantity(Array(1, dtype=int32, ...), unit='rad')
+
+    >>> convert(q, Angle)
+    Angle(Array(1, dtype=int32, ...), unit='rad')
+
+    The self-conversion doesn't copy the object:
+
+    >>> q = Angle(1, "rad")
+    >>> convert(q, Angle) is q
+    True
+
+    """
+    if isinstance(q, Angle):
+        return q
+    return Angle(q.value, q.unit)
---
+++ src/coordinax/angle.py
@@ -0,0 +1,5 @@
+"""Angles."""
+
+__all__ = ["AbstractAngle", "Angle"]
+
+from ._src.angle import AbstractAngle, Angle
