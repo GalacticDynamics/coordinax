e6b5a7d1edc91928f85886cc67cb8bc1e54c8649
nstarman <nstarman@users.noreply.github.com>
2024-10-28 13:50:23 -0400

    docs: example
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_src/angle/base.py
+++ src/coordinax/_src/angle/base.py
@@ -17,6 +17,23 @@ class AbstractAngle(AbstractQuantity):  # type: ignore[misc]
     --------
     `coordinax.angle.Angle` : a concrete implementation of this class.
 
+    Examples
+    --------
+    For this example, we will use the concrete implementation of
+    `coordinax.angle.AbstractAngle`, `coordinax.angle.Angle`.
+
+    >>> from coordinax.angle import Angle
+
+    >>> a = Angle(90, "deg")
+    >>> a
+    Angle(Array(90, dtype=int32, weak_type=True), unit='deg')
+
+    Angles have to have dimensions of angle.
+
+    >>> try: Angle(90, "m")
+    ... except ValueError as e: print(e)
+    Angle must have dimensions angle.
+
     """
 
     def __check_init__(self) -> None:
@@ -38,5 +55,17 @@ add_promotion_rule(AbstractAngle, Quantity, Quantity)
 
 @conversion_method(type_from=AbstractAngle, type_to=Quantity)  # type: ignore[misc]
 def _convert_angle_to_quantity(x: AbstractAngle) -> Quantity:
-    """Convert a distance to a quantity."""
+    """Convert a distance to a quantity.
+
+    Examples
+    --------
+    >>> from unxt import Quantity
+    >>> from coordinax.angle import Angle
+    >>> from plum import convert
+
+    >>> a = Angle(90, "deg")
+    >>> convert(a, Quantity)
+    Quantity['angle'](Array(90, dtype=int32, weak_type=True), unit='deg')
+
+    """
     return Quantity(x.value, x.unit)
