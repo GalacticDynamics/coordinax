e86360ad6225bf799629d790e300191c464b7644
nstarman <nstarman@users.noreply.github.com>
2024-11-19 21:47:07 -0500

    feat: TransformFrameError
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ src/coordinax/_src/frames/errors.py
@@ -0,0 +1,7 @@
+"""Frames sub-package."""
+
+__all__ = ["FrameTransformError"]
+
+
+class FrameTransformError(Exception):
+    """An error occurred during a frame transformation."""
--- src/coordinax/frames.py
+++ src/coordinax/frames.py
@@ -1,15 +1,18 @@
 """Reference frames and transformations between them."""
 
-from . import _coordinax_space_frames
 from ._src.frames.api import frame_transform_op
 from ._src.frames.base import AbstractReferenceFrame
+from ._src.frames.errors import FrameTransformError
 
+# Space frames
 # isort: split
+from . import _coordinax_space_frames
 from ._coordinax_space_frames import *  # noqa: F403
 
 __all__ = [
     "frame_transform_op",
     "AbstractReferenceFrame",
+    "FrameTransformError",
 ]
 __all__ += _coordinax_space_frames.__all__
 
