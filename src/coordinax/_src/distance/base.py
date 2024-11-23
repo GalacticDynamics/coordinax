c0168687a97a1dafa4f6350c210e1c7621e7c311
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-11-23 17:02:18 -0500

    refactor: distance (#240)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/_src/distance/base.py
+++ src/coordinax/_src/distance/base.py
@@ -4,9 +4,7 @@ __all__: list[str] = []
 
 from abc import abstractmethod
 
-from plum import add_promotion_rule, conversion_method
-
-from unxt.quantity import AbstractQuantity, Quantity
+from unxt.quantity import AbstractQuantity
 
 
 class AbstractDistance(AbstractQuantity):  # type: ignore[misc]
@@ -26,19 +24,3 @@ class AbstractDistance(AbstractQuantity):  # type: ignore[misc]
     @abstractmethod
     def distance_modulus(self) -> "DistanceModulus":
         """The distance modulus."""
-
-
-# ============================================================================
-# Conversion and Promotion
-
-# Add a rule that when a AbstractDistance interacts with a Quantity, the
-# distance degrades to a Quantity. This is necessary for many operations, e.g.
-# division of a distance by non-dimensionless quantity where the resulting units
-# are not those of a distance.
-add_promotion_rule(AbstractDistance, Quantity, Quantity)
-
-
-@conversion_method(type_from=AbstractDistance, type_to=Quantity)  # type: ignore[misc]
-def _convert_distance_to_quantity(x: AbstractDistance) -> Quantity:
-    """Convert a distance to a quantity."""
-    return Quantity(x.value, x.unit)
--- src/coordinax/_src/distance/compat.py
+++ src/coordinax/_src/distance/compat.py
@@ -2,14 +2,27 @@
 
 __all__: list[str] = []
 
-from plum import conversion_method
+from plum import add_promotion_rule, conversion_method
 
-from unxt.quantity import AbstractQuantity
+from unxt.quantity import AbstractQuantity, Quantity
 
+from .base import AbstractDistance
 from .core import Distance, DistanceModulus, Parallax
+
+# Add a rule that when a AbstractDistance interacts with a Quantity, the
+# distance degrades to a Quantity. This is necessary for many operations, e.g.
+# division of a distance by non-dimensionless quantity where the resulting units
+# are not those of a distance.
+add_promotion_rule(AbstractDistance, Quantity, Quantity)
 
 #####################################################################
 # Conversion
+
+
+@conversion_method(type_from=AbstractDistance, type_to=Quantity)  # type: ignore[misc]
+def _convert_distance_to_quantity(x: AbstractDistance) -> Quantity:
+    """Convert a distance to a quantity."""
+    return Quantity(x.value, x.unit)
 
 
 @conversion_method(type_from=AbstractQuantity, type_to=Distance)  # type: ignore[misc]
