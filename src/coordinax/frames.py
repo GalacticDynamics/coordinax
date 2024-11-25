b25fe7644f87ed33107f5a9ed77ee1d787f3e591
nstarman <nstarman@users.noreply.github.com>
2024-11-24 23:02:43 -0500

    refactor: clean up private API for frames
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/coordinax/frames.py
+++ src/coordinax/frames.py
@@ -1,15 +1,17 @@
 """Reference frames and transformations between them."""
 
-from ._src.frames.api import frame_transform_op
-from ._src.frames.base import AbstractReferenceFrame
-from ._src.frames.errors import FrameTransformError
-from ._src.frames.null import NoFrame
+from ._src.frames import (
+    AbstractReferenceFrame,
+    FrameTransformError,
+    NoFrame,
+    frame_transform_op,
+)
 
 # Register the frame transform operations
 # isort: split
 from ._src.frames.xfm import *  # noqa: F403
 
-# Space frames
+# Frames from external packages
 # isort: split
 from . import _coordinax_space_frames
 from ._coordinax_space_frames import *  # noqa: F403
