f76827e85a758c2d9638304d993f03086ed84565
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-12-06 18:30:00 -0500

    ✨ feat: enable runtime typechecking for submodules (#270)

--- src/coordinax/angle.py
+++ src/coordinax/angle.py
@@ -2,4 +2,12 @@
 
 __all__ = ["AbstractAngle", "Angle"]
 
-from ._src.angles import AbstractAngle, Angle
+from jaxtyping import install_import_hook
+
+from .setup_package import RUNTIME_TYPECHECKER
+
+with install_import_hook("coordinax.angle", RUNTIME_TYPECHECKER):
+    from ._src.angles import AbstractAngle, Angle
+
+
+del RUNTIME_TYPECHECKER, install_import_hook
--- src/coordinax/distance.py
+++ src/coordinax/distance.py
@@ -2,4 +2,12 @@
 
 __all__ = ["AbstractDistance", "Distance", "DistanceModulus", "Parallax"]
 
-from ._src.distances import AbstractDistance, Distance, DistanceModulus, Parallax
+from jaxtyping import install_import_hook
+
+from .setup_package import RUNTIME_TYPECHECKER
+
+with install_import_hook("coordinax.distance", RUNTIME_TYPECHECKER):
+    from ._src.distances import AbstractDistance, Distance, DistanceModulus, Parallax
+
+
+del RUNTIME_TYPECHECKER, install_import_hook
