2b7e372a821496517dcd4dc98741c632d35efc25
nstarman <nstarman@users.noreply.github.com>
2024-12-06 18:23:16 -0500

    ✨ feat: enable runtime typechecking for submodules

--- src/coordinax/frames.py
+++ src/coordinax/frames.py
@@ -1,28 +1,35 @@
 """Reference frames and transformations between them."""
 
-from ._src.frames import (
-    AbstractReferenceFrame,
-    FrameTransformError,
-    NoFrame,
-    frame_transform_op,
-)
-
-# Register the frame transform operations
-# isort: split
-from ._src.frames.xfm import *  # noqa: F403
-
-# Frames from external packages
-# isort: split
-from . import _coordinax_space_frames
-from ._coordinax_space_frames import *  # noqa: F403
-
 __all__ = [
     "AbstractReferenceFrame",
     "FrameTransformError",
     "NoFrame",
     "frame_transform_op",
 ]
+
+from jaxtyping import install_import_hook
+
+from .setup_package import RUNTIME_TYPECHECKER
+
+with install_import_hook("coordinax.frames", RUNTIME_TYPECHECKER):
+    from ._src.frames import (
+        AbstractReferenceFrame,
+        FrameTransformError,
+        NoFrame,
+        frame_transform_op,
+    )
+
+    # Register the frame transform operations
+    # isort: split
+    from ._src.frames.xfm import *  # noqa: F403
+
+    # Frames from external packages
+    # isort: split
+    from . import _coordinax_space_frames
+    from ._coordinax_space_frames import *  # noqa: F403
+
+
 __all__ += _coordinax_space_frames.__all__
 
 # clean up namespace
-del _coordinax_space_frames
+del _coordinax_space_frames, RUNTIME_TYPECHECKER, install_import_hook
