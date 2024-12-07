0694e63e1476885c6b5ea876cd9109fa09c144e0
nstarman <nstarman@users.noreply.github.com>
2024-12-07 17:42:31 -0500

    🚚 resource(vecs): rename transform file

--- src/coordinax/_src/vectors/transform/__init__.py
+++ src/coordinax/_src/vectors/transform/__init__.py
@@ -6,6 +6,6 @@ from .accelerations import *
 from .d1 import *
 from .d2 import *
 from .d3 import *
-from .differentials import *
 from .dn import *
 from .space import *
+from .velocities import *
