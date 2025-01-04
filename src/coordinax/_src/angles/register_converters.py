19c5ae5b98009ab4eee930094a3f7208409eb635
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-03 18:05:46 -0800

    🎨 style(angle): improve converters

--- src/coordinax/_src/angles/register_converters.py
+++ src/coordinax/_src/angles/register_converters.py
@@ -5,48 +5,9 @@ __all__: list[str] = []
 from plum import conversion_method
 
 import unxt as u
-from unxt.quantity import AbstractQuantity, UncheckedQuantity
+from unxt.quantity import AbstractQuantity
 
 from .angle import Angle
-from .base import AbstractAngle
-
-
-@conversion_method(type_from=AbstractAngle, type_to=u.Quantity)  # type: ignore[misc]
-def convert_angle_to_quantity(x: AbstractAngle) -> u.Quantity:
-    """Convert a distance to a quantity.
-
-    Examples
-    --------
-    >>> import unxt as u
-    >>> from coordinax.angle import Angle
-    >>> from plum import convert
-
-    >>> a = Angle(90, "deg")
-    >>> convert(a, u.Quantity)
-    Quantity['angle'](Array(90, dtype=int32, weak_type=True), unit='deg')
-
-    """
-    unit = u.unit_of(x)
-    return u.Quantity(x.ustrip(unit), unit)
-
-
-@conversion_method(type_from=AbstractAngle, type_to=UncheckedQuantity)  # type: ignore[misc]
-def convert_angle_to_uncheckedquantity(x: AbstractAngle) -> UncheckedQuantity:
-    """Convert a distance to a quantity.
-
-    Examples
-    --------
-    >>> from unxt.quantity import UncheckedQuantity
-    >>> from coordinax.angle import Angle
-    >>> from plum import convert
-
-    >>> a = Angle(90, "deg")
-    >>> convert(a, UncheckedQuantity)
-    UncheckedQuantity(Array(90, dtype=int32, weak_type=True), unit='deg')
-
-    """
-    unit = u.unit_of(x)
-    return UncheckedQuantity(x.ustrip(unit), unit)
 
 
 @conversion_method(type_from=AbstractQuantity, type_to=Angle)  # type: ignore[misc]
