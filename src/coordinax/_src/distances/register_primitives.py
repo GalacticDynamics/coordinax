8cc5821d871495478602a39f9044a8829db9e819
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-21 16:35:07 -0500

    ⚡️ perf(angles): dispatches use UncheckedQuantity (#346)

--- src/coordinax/_src/distances/register_primitives.py
+++ src/coordinax/_src/distances/register_primitives.py
@@ -10,7 +10,7 @@ from jaxtyping import ArrayLike
 from quax import register
 
 import unxt as u
-from unxt.quantity import AbstractQuantity
+from unxt.quantity import AbstractQuantity, UncheckedQuantity as FastQ
 
 from .base import AbstractDistance
 
@@ -22,7 +22,7 @@ radian = u.unit("radian")
 
 # TODO: can this be done with promotion/conversion instead?
 @register(lax.cbrt_p)
-def _cbrt_p_d(x: AbstractDistance) -> u.Quantity:
+def _cbrt_p_d(x: AbstractDistance) -> FastQ:
     """Cube root of a distance.
 
     Examples
@@ -31,10 +31,10 @@ def _cbrt_p_d(x: AbstractDistance) -> u.Quantity:
     >>> from coordinax.distance import Distance
     >>> d = Distance(8, "m")
     >>> jnp.cbrt(d)
-    Quantity['m1/3'](Array(2., dtype=float32, ...), unit='m(1/3)')
+     UncheckedQuantity(Array(2., dtype=float32, weak_type=True), unit='m(1/3)')
 
     """
-    return u.Quantity(lax.cbrt(x.value), unit=x.unit ** (1 / 3))
+    return FastQ(lax.cbrt(x.value), unit=x.unit ** (1 / 3))
 
 
 # ==============================================================================
@@ -43,7 +43,7 @@ def _cbrt_p_d(x: AbstractDistance) -> u.Quantity:
 @register(lax.dot_general_p)
 def _dot_general_dd(
     lhs: AbstractDistance, rhs: AbstractDistance, /, **kwargs: Any
-) -> u.Quantity:
+) -> FastQ:
     """Dot product of two Distances.
 
     Examples
@@ -57,9 +57,9 @@ def _dot_general_dd(
     >>> q1 = Distance([1, 2, 3], "m")
     >>> q2 = Distance([4, 5, 6], "m")
     >>> jnp.vecdot(q1, q2)
-    Quantity['area'](Array(32, dtype=int32), unit='m2')
+    UncheckedQuantity(Array(32, dtype=int32), unit='m2')
     >>> q1 @ q2
-    Quantity['area'](Array(32, dtype=int32), unit='m2')
+    UncheckedQuantity(Array(32, dtype=int32), unit='m2')
 
     This rule is also used by `jnp.matmul` for quantities.
 
@@ -74,17 +74,15 @@ def _dot_general_dd(
     Quantity['length'](Array([0, 1, 0], dtype=int32), unit='m')
 
     """
-    return u.Quantity(
-        lax.dot_general_p.bind(lhs.value, rhs.value, **kwargs),
-        unit=lhs.unit * rhs.unit,
-    )
+    value = lax.dot_general_p.bind(lhs.value, rhs.value, **kwargs)
+    return FastQ(value, unit=lhs.unit * rhs.unit)
 
 
 # ==============================================================================
 
 
 @register(lax.integer_pow_p)
-def _integer_pow_p_d(x: AbstractDistance, *, y: Any) -> u.Quantity:
+def _integer_pow_p_d(x: AbstractDistance, *, y: Any) -> FastQ:
     """Integer power of a Distance.
 
     Examples
@@ -92,17 +90,17 @@ def _integer_pow_p_d(x: AbstractDistance, *, y: Any) -> u.Quantity:
     >>> from coordinax.distance import Distance
     >>> q = Distance(2, "m")
     >>> q ** 3
-    Quantity['volume'](Array(8, dtype=int32, ...), unit='m3')
+     UncheckedQuantity(Array(8, dtype=int32, weak_type=True), unit='m3')
 
     """
-    return u.Quantity(value=lax.integer_pow(x.value, y), unit=x.unit**y)
+    return FastQ(value=lax.integer_pow(x.value, y), unit=x.unit**y)
 
 
 # ==============================================================================
 
 
 @register(lax.pow_p)
-def _pow_p_d(x: AbstractDistance, y: ArrayLike) -> u.Quantity:
+def _pow_p_d(x: AbstractDistance, y: ArrayLike) -> FastQ:
     """Power of a Distance by redispatching to Quantity.
 
     Examples
@@ -113,17 +111,17 @@ def _pow_p_d(x: AbstractDistance, y: ArrayLike) -> u.Quantity:
     >>> q1 = Distance(10.0, "m")
     >>> y = 3.0
     >>> q1 ** y
-    Quantity['volume'](Array(1000., dtype=float32, ...), unit='m3')
+    UncheckedQuantity(Array(1000., dtype=float32, weak_type=True), unit='m3')
 
     """
-    return u.Quantity(x.value, x.unit) ** y  # TODO: better call to power
+    return FastQ(x.value, x.unit) ** y  # TODO: better call to power
 
 
 # ==============================================================================
 
 
 @register(lax.sqrt_p)
-def _sqrt_p_d(x: AbstractDistance) -> u.Quantity:
+def _sqrt_p_d(x: AbstractDistance) -> FastQ:
     """Square root of a quantity.
 
     Examples
@@ -133,16 +131,16 @@ def _sqrt_p_d(x: AbstractDistance) -> u.Quantity:
     >>> from coordinax.distance import Distance
     >>> q = Distance(9, "m")
     >>> jnp.sqrt(q)
-    Quantity['m0.5'](Array(3., dtype=float32, ...), unit='m(1/2)')
+    UncheckedQuantity(Array(3., dtype=float32, weak_type=True), unit='m(1/2)')
 
     >>> from coordinax.distance import Parallax
     >>> q = Parallax(9, "mas")
     >>> jnp.sqrt(q)
-    Quantity['rad0.5'](Array(3., dtype=float32, ...), unit='mas(1/2)')
+    UncheckedQuantity(Array(3., dtype=float32, weak_type=True), unit='mas(1/2)')
 
     """
     # Promote to something that supports sqrt units.
-    return u.Quantity(lax.sqrt(x.value), unit=x.unit ** (1 / 2))
+    return FastQ(lax.sqrt(x.value), unit=x.unit ** (1 / 2))
 
 
 # ==============================================================================
@@ -154,5 +152,5 @@ def _to_value_rad_or_one(q: AbstractQuantity) -> ArrayLike:
 
 # TODO: figure out a promotion alternative that works in general
 @register(lax.tan_p)
-def _tan_p_d(x: AbstractDistance) -> u.Quantity["dimensionless"]:
-    return u.Quantity(lax.tan(_to_value_rad_or_one(x)), unit=one)
+def _tan_p_d(x: AbstractDistance) -> FastQ:
+    return FastQ(lax.tan(_to_value_rad_or_one(x)), unit=one)
