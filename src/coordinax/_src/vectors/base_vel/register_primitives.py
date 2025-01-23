b20ba0f43ab8ac34d8a01e9f67eec57083929656
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-22 19:42:11 -0500

    ♻️ refactor(vectors): base_vel
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/coordinax/_src/vectors/base_vel/register_primitives.py
+++ src/coordinax/_src/vectors/base_vel/register_primitives.py
@@ -7,6 +7,7 @@ from typing import cast
 import jax
 from quax import register
 
+import quaxed.numpy as jnp
 import unxt as u
 from dataclassish import field_items
 
@@ -39,3 +40,34 @@ def _mul_vel_q(self: AbstractVel, other: u.Quantity["time"]) -> AbstractPos:
     """
     fs = {k[2:]: v * other for k, v in field_items(self)}
     return cast(AbstractPos, self.integral_cls.from_(fs))
+
+
+# -----------------------------------------------
+
+
+@register(jax.lax.neg_p)
+def neg_vel(vec: AbstractVel, /) -> AbstractVel:
+    """Negate the vector.
+
+    Examples
+    --------
+    >>> from quaxed import lax
+    >>> import unxt as u
+    >>> import coordinax as cx
+
+    >>> dr = cx.vecs.RadialVel.from_([1], "m/s")
+    >>> print(lax.neg(dr))
+    <RadialVel (d_r[m / s])
+        [-1]>
+
+    >>> -dr
+    RadialVel( d_r=Quantity[...]( value=i32[], unit=Unit("m / s") ) )
+
+    >>> dp = cx.vecs.PolarVel(u.Quantity(1, "m/s"), u.Quantity(1, "mas/yr"))
+    >>> neg_dp = -dp
+    >>> print(neg_dp)
+    <PolarVel (d_r[m / s], d_phi[mas / yr])
+        [-1 -1]>
+
+    """
+    return jax.tree.map(jnp.negative, vec)
