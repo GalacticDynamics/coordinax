6cb96404a455ea7626931791775fa8915aeaf80a
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-02-27 19:58:33 -0500

    ♻️ refactor(vecs): velocity transformation
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/coordinax/_src/vectors/register_vconvert/time_derivatives.py
+++ src/coordinax/_src/vectors/register_vconvert/time_derivatives.py
@@ -3,7 +3,6 @@
 __all__: list[str] = []
 
 from dataclasses import replace
-from functools import partial
 from math import prod
 from typing import Any, cast
 
@@ -16,12 +15,30 @@ import unxt as u
 from dataclassish import field_items
 from unxt.quantity import is_any_quantity
 
+from .common import get_params_and_aux
 from coordinax._src.distances import AbstractDistance
 from coordinax._src.vectors import d1, d2, d3
-from coordinax._src.vectors.base import AttrFilter
 from coordinax._src.vectors.base_acc import AbstractAcc
 from coordinax._src.vectors.base_pos import AbstractPos
 from coordinax._src.vectors.base_vel import AbstractVel
+
+
+def transform_jac(
+    jac: dict[str, u.AbstractQuantity],
+) -> dict[str, dict[str, u.AbstractQuantity]]:
+    """Transform Jacobian.
+
+    It comes in as  ``{k: Quantity({kk: Quantity(vv, unit2)}, unit)}``
+    it needs to be rearranged to ``{k: {kk: Quantity(vv, unit/unit2)}}``.
+
+    """
+    return {
+        out_k: {
+            k: u.Quantity(v.value, out_v.unit / v.unit) for k, v in out_v.value.items()
+        }
+        for out_k, out_v in jac.items()
+    }
+
 
 is_leaf = lambda x: is_any_quantity(x) or eqx.is_array(x)  # noqa: E731
 
@@ -160,61 +177,33 @@ def vconvert(
     # RadialVel, we need the Jacobian of the CartesianPos1D -> RadialPos so the
     # position must be transformed to CartesianPos1D.
     from_posv = vconvert(in_pos_cls, from_posv_, **kwargs)
-    # The Jacobian requires the position to be a float
+    # Jacobian requires the position to be a float
     dtype = jnp.result_type(float)  # TODO: better match e.g. int16 to float16?
     from_posv = from_posv.astype(dtype, copy=False)  # cast to float
 
-    # TODO: not need to cast to distance
-    from_posv = replace(
-        from_posv,
-        **{
-            k: v.distance
-            for k, v in field_items(AttrFilter, from_posv)
-            if isinstance(v, AbstractDistance)
-        },
-    )
-
-    # Takes the Jacobian through the representation transformation function.  This
-    # returns a representation of the target type, where the value of each field the
-    # corresponding row of the Jacobian. The value of the field is a Quantity with
-    # the correct numerator unit (of the Jacobian row). The value is a Vector of the
-    # original type, with fields that are the columns of that row, but with only the
-    # denomicator's units.
-    tmp = partial(vconvert, **kwargs)
-    jac_rep_as = eqx.filter_jit(jax.vmap(jax.jacfwd(tmp, argnums=1), in_axes=(None, 0)))
-    jac_nested_vecs = jac_rep_as(out_pos_cls, from_posv)
-
-    # This changes the Jacobian to be a dictionary of each row, with the value
-    # being that row's column as a dictionary, now with the correct units for
-    # each element:  {row_i: {col_j: Quantity(value, row.unit / column.unit)}}
-    jac_rows = {
-        k: {
-            kk: u.Quantity(vv.value, unit=v.unit / vv.unit)
-            for kk, vv in field_items(AttrFilter, v.value)
-        }
-        for k, v in field_items(AttrFilter, jac_nested_vecs)
+    # Convert to a dictionary of parameters and auxiliary values.
+    in_pos_params, in_pos_aux = get_params_and_aux(from_posv)
+    in_pos_params = {  # NOTE: if use unitful jacobian, this is not needed
+        k: (v.distance if isinstance(v, AbstractDistance) else v)
+        for k, v in in_pos_params.items()
     }
 
-    # Now we can use the Jacobian to transform the differential.
-    flat_current = from_vel.reshape(flat_shape)
-    newvec = to_vel_vector(
-        **{  # Each field is the dot product of the row of the J and the diff column.
-            k: jnp.sum(  # Doing the dot product.
-                jnp.stack(
-                    tuple(j_c * getattr(flat_current, kk) for kk, j_c in j_r.items())
-                ),
-                axis=0,
-            )
-            for k, j_r in jac_rows.items()
-        }
+    jac, out_pos_aux = jax.jacfwd(vconvert, argnums=2, has_aux=True)(
+        out_pos_cls, in_pos_cls, in_pos_params, in_aux=in_pos_aux
     )
+    jac = transform_jac(jac)
+
+    to_vel_params = _dot_jac_vec(jac, from_vel.asdict())
+
+    # TODO: what about the auxiliary values?
+    to_vel = to_vel_vector(**to_vel_params)
 
     # Reshape the output to the original shape
-    newvec = newvec.reshape(shape)
+    to_vel = to_vel.reshape(shape)
 
     # TODO: add  df(q)/dt, which is 0 for all current transforms
 
-    return newvec  # noqa: RET504
+    return to_vel  # noqa: RET504
 
 
 # TODO: implement for cross-representations
