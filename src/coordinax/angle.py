5512b246cf2bf919db49e443e5662f85d44d1aa7
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-05-26 23:52:38 -0700

    ✨ feat(angle): add wrapping
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/coordinax/angle.py
+++ src/coordinax/angle.py
@@ -1,13 +1,13 @@
 """`coordinax.angle` module."""
 
-__all__ = ["AbstractAngle", "Angle", "Parallax"]
+__all__ = ["AbstractAngle", "Angle", "wrap_to", "Parallax"]
 
 from jaxtyping import install_import_hook
 
 from .setup_package import RUNTIME_TYPECHECKER
 
 with install_import_hook("coordinax.angle", RUNTIME_TYPECHECKER):
-    from ._src.angles import AbstractAngle, Angle
+    from ._src.angles import AbstractAngle, Angle, wrap_to
     from ._src.distances import Parallax
 
 
