d05901bbe72b1e0f89ff3537766adf7318e0738c
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-06-01 22:47:45 -0700

    Misc (#422)
    * 🎨 style(frames): internal frame_of in __all__
    * 📝 docs(frames): frame_transform_op
    * 🎨 style(frames): coordinate
    * 🎨 style(frames): VectorAttribute in __all__
    * 🎨 style(vecs): AttrFilter in __all__
    
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/coordinax/_src/frames/api.py
+++ src/coordinax/_src/frames/api.py
@@ -1,6 +1,6 @@
 """Frames sub-package."""
 
-__all__ = ["frame_transform_op"]
+__all__ = ["frame_transform_op", "frame_of"]
 
 from typing import Annotated as Antd, Any
 from typing_extensions import Doc
@@ -14,7 +14,26 @@ def frame_transform_op(
     to_frame: Antd[Any, Doc("frame to convert to")],
     /,
 ) -> Any:
-    """Make a frame transform."""
+    """Make a frame transform.
+
+    Examples
+    --------
+    >>> import coordinax.vecs as cxv
+    >>> import coordinax.frames as cxf
+
+    >>> icrs = cxf.ICRS()
+    >>> gcf = cxf.Galactocentric()
+
+    >>> op = cxf.frame_transform_op(icrs, gcf)
+    >>> op
+    Pipe(( ... ))
+
+    >>> q = cxv.CartesianPos3D.from_([1, 2, 3], "kpc")
+    >>> print(op(q))
+    <CartesianPos3D: (x, y, z) [kpc]
+        [-11.375   1.845   0.133]>
+
+    """
 
 
 @dispatch.abstract
--- src/coordinax/_src/vectors/base/attribute.py
+++ src/coordinax/_src/vectors/base/attribute.py
@@ -1,6 +1,6 @@
 """Representation of coordinates in different systems."""
 
-__all__: list[str] = []
+__all__ = ["VectorAttribute"]
 
 from collections.abc import Callable
 from dataclasses import KW_ONLY, dataclass, field, is_dataclass
