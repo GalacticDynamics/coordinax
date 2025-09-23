868611a7bf9172497b3506f6204d0bb46c7c9be4
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2025-09-23 15:17:18 -0400

    update Translate implementation and docstrings

--- src/coordinax/_src/operators/translate.py
+++ src/coordinax/_src/operators/translate.py
@@ -3,7 +3,7 @@
 __all__ = ["Translate"]
 
 
-from typing import Any, Literal, cast, final
+from typing import Any, Literal, final
 
 import equinox as eqx
 from plum import convert, dispatch
@@ -16,7 +16,6 @@ from coordinax._src.operators.base import AbstractOperator
 from coordinax._src.operators.identity import Identity
 from coordinax._src.vectors import api
 from coordinax._src.vectors.base_pos import AbstractPos
-from coordinax._src.vectors.d3 import AbstractPos3D
 
 
 @final
@@ -46,17 +45,12 @@ class Translate(AbstractOperator):
 
     We can then create a translation operator:
 
-    >>> op = cx.ops.Translate.from_([1.0, 2.0, 3.0, 4.0], "km")
+    >>> op = cx.ops.Translate.from_([1.0, 2.0, 3.0], "km")
     >>> op
     Translate(
       delta_t=Quantity(f32[], unit='s'),
       delta_q=CartesianPos3D( ... )
     )
-
-    TODO: actually this isn't right - it does some four-vector magic I don't like!
-    Here, no time translation is specified, so it defaults to zero (in seconds, but the
-    unit does not matter).
-
     Note that position translation is a `coordinax.vecs.CartesianPos3D`, which was
     constructed from a 1D array, using :meth:`coordinax.vecs.AbstractPos.from_`.  We can
     also construct it directly, which allows for other vector types:
@@ -84,19 +78,13 @@ class Translate(AbstractOperator):
 
     """
 
-    delta_t: u.Quantity["time"] = eqx.field(converter=u.Quantity.from_)
-    """The time translation.
+    delta: AbstractPos = eqx.field(converter=Unless(AbstractPos, api.vector))
+    """The translation amount.
 
-    This parameter uses :meth:`unxt.Quantity.from_` to enable a variety of more
-    convenient input types. See `unxt.Quantity` for details.
-    """
-
-    delta_q: AbstractPos = eqx.field(converter=Unless(AbstractPos, api.vector))
-    """The spatial translation.
-
-    This parameters uses :meth:`coordinax.vecs.CartesianPos3D.from_` to enable a
-    variety of more convenient input types. See `coordinax.vecs.CartesianPos3D`
-    for details.
+    This supports both position translations, by passing in an `AbstractPos`
+    subclass or a `Quantity` with length unit, and time translations, by passing in a
+    `u.Quantity` with a time unit. This also supports a simultaneous translation in
+    position and time by specifying a `FourVector`
     """
 
     # -------------------------------------------
@@ -137,19 +125,17 @@ class Translate(AbstractOperator):
         )
 
         """
-        delta_q = cast("AbstractPos3D", -self.delta_q)
-        return Translate(-self.delta_t, delta_q)
+        return Translate(-self.delta)
 
     # -------------------------------------------
 
     @AbstractOperator.__call__.dispatch  # type: ignore[misc]
     def __call__(
         self: "Translate",
-        t: u.Quantity["time"],
-        x: AbstractPos3D,
+        x: AbstractPos,
         /,
         **__: Any,
-    ) -> tuple[u.Quantity["time"], AbstractPos3D]:
+    ) -> AbstractPos:
         """Apply the translation to the coordinates.
 
         Examples
@@ -158,26 +144,64 @@ class Translate(AbstractOperator):
         >>> import coordinax as cx
         >>> import coordinax.ops as cxo
 
-        Explicitly construct the translation operator:
+        For positions only:
 
-        >>> qshift = cx.CartesianPos3D.from_([1, 1, 1], "km")
-        >>> tshift = u.Quantity(1, "Gyr")
-        >>> op = cx.ops.Translate(tshift, qshift)
-
-        Construct a vector to translate
-
+        >>> dq = cx.CartesianPos3D.from_([1, 1, 1], "km")
+        >>> op = cx.ops.Translate(dq)
         >>> q = cx.CartesianPos3D.from_([1, 2, 3], "km")
-        >>> t = u.Quantity(1, "Gyr")
-        >>> newt, newq = op(t, q)
-
+        >>> newq = op(q)
         >>> newq.x
         Quantity(Array(2, dtype=int32), unit='km')
+        >>> newq.y
+        Quantity(Array(3, dtype=int32), unit='km')
+        >>> newq.z
+        Quantity(Array(4, dtype=int32), unit='km')
 
-        >>> newt
-        Quantity(Array(2, dtype=int32, ...), unit='Gyr')
+        This also works for position + time translations using a `FourVector`:
+
+        >>> dq4 = cx.FourVector(t=u.Quantity(1, "s"), q=u.Quantity([4, 5, 6], "m"))
+        >>> op4 = cx.ops.Translate(dq4)
+
+        Now let's apply it to a `FourVector`:
+
+        >>> q4 = cx.FourVector(t=u.Quantity(0, "s"), q=u.Quantity([1, 2, 3], "m"))
+        >>> newq4 = op4(q4)
+        >>> newq4
+        FourVector( t=Quantity(...), q=CartesianPos3D( ... ) )
+
+        >>> newq4.q.x
+        Quantity(Array(5., dtype=float32), unit='km')
+        >>> newq4.t
+        Quantity(Array(1., dtype=float32), unit='s')
+
+        TODO: Now on a VelocityBoost:
+
+        >>> op = cx.ops.VelocityBoost.from_([1, 2, 3], "m/s")
+
+        >>> v4 = cx.FourVector.from_([0, 0, 0, 0], "m")
+        >>> newv4 = op(v4)
+        >>> print(newv4)
+        <FourVector: (t[m s / km], q=(x, y, z) [m])
+            [0. 0. 0. 0.]>
 
         """
-        return t + self.delta_t, x + self.delta_q
+        return x + self.delta
+
+
+@AbstractOperator.from_.dispatch  # type: ignore[misc]
+def from_(cls: type[Translate], delta: u.Quantity["length"], /) -> Translate:
+    """Construct from a spatial offset.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import coordinax.ops as cxo
+
+    >>> q = u.Quantity([1.0, 2.0, 3.0], "km")
+    >>> op = cxo.Translation.from_(q)
+
+    """
+    return cls(delta=api.vector(delta))
 
 
 # -------------------------------------------
@@ -205,15 +229,13 @@ def simplify_op(op: Translate, /, **kwargs: Any) -> Translate | Identity:
 
     """
     # Check if the translation is zero.
-    q = convert(op.delta_q, u.Quantity).value
-    if jnp.allclose(op.delta_t.value, 0, **kwargs) and jnp.allclose(q, 0, **kwargs):
+    q = convert(op.delta, u.Quantity)
+    if jnp.allclose(q.value, 0, **kwargs):
         return Identity()
-    # TODO: Check if the translation is purely spatial.
 
     return op
 
 
-# TODO: show op3.translation = op1.translation + op2.translation
 @dispatch
 def simplify_op(op1: Translate, op2: Translate, /) -> Translate:
     """Combine two translations into a single translation.
@@ -224,18 +246,16 @@ def simplify_op(op1: Translate, op2: Translate, /) -> Translate:
     >>> import coordinax as cx
 
     >>> qshift = cx.CartesianPos3D.from_([1, 0, 0], "km")
-    >>> tshift = u.Quantity(1, "Gyr")
-    >>> op1 = cx.ops.Translate(tshift, qshift)
+    >>> op1 = cx.ops.Translate(qshift)
 
-    >>> qshift = cx.CartesianPos3D.from_([0, 1, 0], "km")
-    >>> op2 = cx.ops.Translate(tshift, qshift)
+    >>> qshift2 = cx.CartesianPos3D.from_([0, 1, 0], "km")
+    >>> op2 = cx.ops.Translate(qshift2)
 
     >>> op3 = cx.ops.simplify_op(op1, op2)
     >>> op3
     Translate(
-      delta_t=Quantity(weak_i32[], unit='Gyr'),
-      delta_q=CartesianPos3D( ... )
+      delta=CartesianPos3D( ... )
     )
 
     """
-    return Translate(op1.delta_t + op2.delta_t, op1.delta_q + op2.delta_q)
+    return Translate(op1.delta + op2.delta)
