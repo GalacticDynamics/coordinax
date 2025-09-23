95bccd04bad4765fcfcd1f92b9a573b0918f2acc
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2025-09-23 14:03:56 -0400

    rename GalileanTranslation to Translate

--- src/coordinax/_src/operators/translate.py
+++ src/coordinax/_src/operators/translate.py
@@ -1,7 +1,6 @@
-# ruff: noqa: ERA001
-"""Galilean coordinate transformations."""
+"""Coordinate translations."""
 
-__all__ = ["GalileanTranslation"]
+__all__ = ["Translate"]
 
 
 from typing import Any, Literal, cast, final
@@ -13,17 +12,16 @@ import quaxed.numpy as jnp
 import unxt as u
 from dataclassish.converters import Unless
 
-from .base import AbstractGalileanOperator
 from coordinax._src.operators.base import AbstractOperator
 from coordinax._src.operators.identity import Identity
 from coordinax._src.vectors import api
 from coordinax._src.vectors.base_pos import AbstractPos
-from coordinax._src.vectors.d3 import AbstractPos3D, CartesianPos3D
+from coordinax._src.vectors.d3 import AbstractPos3D
 
 
 @final
-class GalileanTranslation(AbstractGalileanOperator):
-    r"""Operator for spatio-temporal translations.
+class Translate(AbstractOperator):
+    r"""Operator for spatial and temporal translations.
 
     The coordinate transform is given by:
 
@@ -31,9 +29,7 @@ class GalileanTranslation(AbstractGalileanOperator):
 
         (t,\mathbf{x}) \mapsto (t+s, \mathbf{x} + \mathbf {a})
 
-    where :math:`a \in R^3` and :math:`s \in R`.  Therefore for a potential
-    :math:`\Phi(t,\mathbf{x})` in the translated frame the potential is given by
-    the subtraction of the translation.
+    where :math:`a \in R^3` and :math:`s \in R`.
 
     Parameters
     ----------
@@ -50,29 +46,32 @@ class GalileanTranslation(AbstractGalileanOperator):
 
     We can then create a translation operator:
 
-    >>> op = cx.ops.GalileanTranslation.from_([1.0, 2.0, 3.0, 4.0], "km")
+    >>> op = cx.ops.Translate.from_([1.0, 2.0, 3.0, 4.0], "km")
     >>> op
-    GalileanTranslation(
+    Translate(
       delta_t=Quantity(f32[], unit='s'),
       delta_q=CartesianPos3D( ... )
     )
 
-    Note that the translation is a `unxt.Quantity` and a
-    `coordinax.vecs.AbstractPos`, which was constructed from a 1D array, using
-    :meth:`coordinax.vecs.AbstractPos.from_`.  We can also construct it
-    directly, which allows for other vector types.
+    TODO: actually this isn't right - it does some four-vector magic I don't like!
+    Here, no time translation is specified, so it defaults to zero (in seconds, but the
+    unit does not matter).
+
+    Note that position translation is a `coordinax.vecs.CartesianPos3D`, which was
+    constructed from a 1D array, using :meth:`coordinax.vecs.AbstractPos.from_`.  We can
+    also construct it directly, which allows for other vector types:
 
     >>> qshift = cx.SphericalPos(r=u.Quantity(1.0, "km"),
     ...                          theta=u.Quantity(jnp.pi/2, "rad"),
     ...                          phi=u.Quantity(0, "rad"))
-    >>> op = cx.ops.GalileanTranslation(u.Quantity(1.0, "Gyr"), qshift)
+    >>> op = cx.ops.Translate(u.Quantity(1.0, "Gyr"), qshift)
     >>> op
-    GalileanTranslation(
+    Translate(
       delta_t=Quantity(weak_f32[], unit='Gyr'),
       delta_q=SphericalPos( ... )
     )
 
-    Translation operators can be applied to `coordinax.vecs.AbstractPos` and
+    Translation operators can be applied to `coordinax.vecs.AbstractPos` subclasses and
     `unxt.Quantity`:
 
     >>> q = cx.CartesianPos3D.from_([0, 0, 0], "km")
@@ -104,13 +103,13 @@ class GalileanTranslation(AbstractGalileanOperator):
 
     @property
     def is_inertial(self) -> Literal[True]:
-        """Galilean translation is an inertial-frame preserving transformation.
+        """Translation is an inertial-frame preserving transformation.
 
         Examples
         --------
         >>> import coordinax as cx
 
-        >>> op = cx.ops.GalileanTranslation.from_([0, 1, 1, 1], "km")
+        >>> op = cx.ops.Translate.from_([0, 1, 1, 1], "km")
 
         >>> op.is_inertial
         True
@@ -119,7 +118,7 @@ class GalileanTranslation(AbstractGalileanOperator):
         return True
 
     @property
-    def inverse(self) -> "GalileanTranslation":
+    def inverse(self) -> "Translate":
         """The inverse of the operator.
 
         Examples
@@ -128,24 +127,24 @@ class GalileanTranslation(AbstractGalileanOperator):
         >>> import coordinax as cx
 
         >>> qshift = cx.CartesianPos3D.from_([1, 1, 1], "km")
-        >>> op = cx.ops.GalileanTranslation(u.Quantity(1, "Gyr"), qshift)
+        >>> op = cx.ops.Translate(u.Quantity(1, "Gyr"), qshift)
 
         >>> print(op.inverse)
-        GalileanTranslation(
+        Translate(
             delta_t=Quantity(-1, unit='Gyr'),
             delta_q=<CartesianPos3D: (x, y, z) [km]
                 [-1 -1 -1]>
         )
 
         """
-        delta_q = cast(AbstractPos3D, -self.delta_q)
-        return GalileanTranslation(-self.delta_t, delta_q)
+        delta_q = cast("AbstractPos3D", -self.delta_q)
+        return Translate(-self.delta_t, delta_q)
 
     # -------------------------------------------
 
     @AbstractOperator.__call__.dispatch  # type: ignore[misc]
     def __call__(
-        self: "GalileanTranslation",
+        self: "Translate",
         t: u.Quantity["time"],
         x: AbstractPos3D,
         /,
@@ -163,7 +162,7 @@ class GalileanTranslation(AbstractGalileanOperator):
 
         >>> qshift = cx.CartesianPos3D.from_([1, 1, 1], "km")
         >>> tshift = u.Quantity(1, "Gyr")
-        >>> op = cx.ops.GalileanTranslation(tshift, qshift)
+        >>> op = cx.ops.Translate(tshift, qshift)
 
         Construct a vector to translate
 
@@ -181,35 +180,11 @@ class GalileanTranslation(AbstractGalileanOperator):
         return t + self.delta_t, x + self.delta_q
 
 
-@AbstractOperator.from_.dispatch  # type: ignore[misc]
-def from_(
-    cls: type[GalileanTranslation], delta: u.AbstractQuantity, /
-) -> GalileanTranslation:
-    """Construct from a time+space array.
-
-    Examples
-    --------
-    >>> import unxt as u
-    >>> import coordinax.ops as cxo
-
-    >>> q = u.Quantity([1.0, 2.0, 3.0, 4.0], "km")
-    >>> op = cxo.GalileanTranslation.from_(q)
-
-    """
-    return cls(
-        delta_t=delta[0]
-        / u.Quantity(299_792.458, "km/s"),  # TODO: couple to FourVector value
-        delta_q=CartesianPos3D.from_(delta[1:]),
-    )
-
-
 # -------------------------------------------
 
 
 @dispatch
-def simplify_op(
-    op: GalileanTranslation, /, **kwargs: Any
-) -> GalileanTranslation | Identity:
+def simplify_op(op: Translate, /, **kwargs: Any) -> Translate | Identity:
     """Simplify a Galilean translation operator.
 
     Examples
@@ -218,16 +193,13 @@ def simplify_op(
 
     An operator with real effect cannot be simplified:
 
-    >>> op = cxo.GalileanTranslation.from_([3e8, 1, 0, 0], "m")
+    >>> op = cxo.Translate.from_([1, 0, 0], "m")
     >>> cxo.simplify_op(op)
-    GalileanTranslation(
-      delta_t=Quantity(f32[], unit='m s / km'),
-      delta_q=CartesianPos3D( ... )
-    )
+    Translate(...)
 
     An operator with no effect can be simplified:
 
-    >>> op = cxo.GalileanTranslation.from_([0, 0, 0, 0], "m")
+    >>> op = cxo.Translate.from_([0, 0, 0], "m")
     >>> cxo.simplify_op(op)
     Identity()
 
@@ -243,9 +215,7 @@ def simplify_op(
 
 # TODO: show op3.translation = op1.translation + op2.translation
 @dispatch
-def simplify_op(
-    op1: GalileanTranslation, op2: GalileanTranslation, /
-) -> GalileanTranslation:
+def simplify_op(op1: Translate, op2: Translate, /) -> Translate:
     """Combine two translations into a single translation.
 
     Examples
@@ -255,17 +225,17 @@ def simplify_op(
 
     >>> qshift = cx.CartesianPos3D.from_([1, 0, 0], "km")
     >>> tshift = u.Quantity(1, "Gyr")
-    >>> op1 = cx.ops.GalileanTranslation(tshift, qshift)
+    >>> op1 = cx.ops.Translate(tshift, qshift)
 
     >>> qshift = cx.CartesianPos3D.from_([0, 1, 0], "km")
-    >>> op2 = cx.ops.GalileanTranslation(tshift, qshift)
+    >>> op2 = cx.ops.Translate(tshift, qshift)
 
     >>> op3 = cx.ops.simplify_op(op1, op2)
     >>> op3
-    GalileanTranslation(
+    Translate(
       delta_t=Quantity(weak_i32[], unit='Gyr'),
       delta_q=CartesianPos3D( ... )
     )
 
     """
-    return GalileanTranslation(op1.delta_t + op2.delta_t, op1.delta_q + op2.delta_q)
+    return Translate(op1.delta_t + op2.delta_t, op1.delta_q + op2.delta_q)
